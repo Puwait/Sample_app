8b0f6bfdb4a97d779ee25fc23c5f9411e05a95da
Puwait <purechrisy896@gmail.com>
2018-02-27 11:54:03 +0300

    The Users controller with a show action

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
   get 'static_pages/contact', to: 'static_pages#contact'
   get 'signup'  => 'users#new'
 
+  resources :users
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
