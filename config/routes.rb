f5ac3db2a0d5eb76d9ca2d01e4914fc3676b8944
Puwait <purechrisy896@gmail.com>
2018-03-13 17:40:34 +0300

    create a login session for successful logins and display error message for unsuccessful logins

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
 
 
+  get 'sessions/new'
+
   root 'static_pages#home'
 
  # get 'static_pages/home', to: 'static_pages#home'
@@ -17,8 +19,13 @@ Rails.application.routes.draw do
 
   get '/signup', to: 'users#new'
   #get 'signup' => 'users#new'
-post '/signup', to: 'users#new'
+
+  post '/signup', to: 'users#create'
   #post 'signup' => 'users#create'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 
 
