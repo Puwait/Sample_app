9311387c53e93b47be1009ef40b433818be9fa84
Puwait <purechrisy896@gmail.com>
2018-03-14 11:03:37 +0300

    logging in the user upon signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -22,6 +22,7 @@ test "valid signup information" do test_invalid_signup_information#Minitest::Res
   follow_redirect!
   assert_template 'users/show' 
   assert_not flash.empty?
+ assert is_logged_in? 
 end
 end
 
