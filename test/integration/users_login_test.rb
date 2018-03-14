1cbc98ef0ba251f94b12c56ecd5cb086bddd1d0d
Puwait <purechrisy896@gmail.com>
2018-03-14 13:59:16 +0300

    correct password digest

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -18,7 +18,7 @@ end
 test "login with valid information" do
   get login_path
  post login_path, params: { session: { email:  @user.email, password: 'passworder' } }
-  assert_redirected_to @user
+ assert_redirected_to @user
 follow_redirect!
 assert_template 'users/show'
 assert_select "a[href=?]", login_path, count: 0
@@ -41,7 +41,7 @@ test "login with valid information followed by logout" do
   assert_not is_logged_in?
   assert_redirected_to root_url
   follow_redirect!
-  assert_select "a[href=?]", login_paths
+  assert_select "a[href=?]", login_path
   assert_select "a[href=?]", logout_path, count: 0
   assert_select "a[href=?]", user_path(@user), count: 0
 end
