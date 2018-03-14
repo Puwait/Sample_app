1eda7bec6313294a6c21f5f0dafc93b2b5d18058
Puwait <purechrisy896@gmail.com>
2018-03-14 10:50:58 +0300

    adding a digest method for use in fixtures

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,7 +5,7 @@ module SessionsHelper
     end
         #returns the current logged in user if any
         def current_user
-            @current_user ||= User.find_by(id: session[user_id])
+            @current_user ||= User.find_by(id: session[:user_id])
        end
     #Returns true if the user is logged in, false otherwise.
     def logged_in?
