90b69ebebad8c979bb495f80064e5f4fc5fb10ce
Puwait <purechrisy896@gmail.com>
2018-03-15 13:28:55 +0300

    Remembering and forgetting users

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,9 +19,19 @@ class User < ApplicationRecord
     def self.new_token
         SecureRandom.urlsafe_base64
     end
+    
+    #Remembers a user in the database for use in persistent sessions.
     def remember
         self.remember_token = User.new_token
         update_attribute(:remember_digest, User.digest(remember_token))
     end
 
+    #Returns true if the given token matches the digest
+    def authenticated?(remember_token)
+        BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+#forgets a user
+    def forget
+        update_attribute(:remember_digest, nil)
+    end
 end
